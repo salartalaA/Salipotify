984d91c1e0aebefb88e5f20b44cf11882101426a
salartalaA <salarmhmdn@gmail.com>
2025-02-23 01:54:27 +0330

    work broo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,19 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		port: 3000,
-	},
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
 });
